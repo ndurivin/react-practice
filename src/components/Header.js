6307d525565ad9f80af9c426df1db1c92f293f90
Vincent Nduri <ndurivo@gmail.com>
2023-01-24 10:12:16 +0300

    Adds boilerplate code to components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='container'>
+        <h2>This is the Header Page</h2>
+    </div>
+  )
+}
+
+export default Header;
